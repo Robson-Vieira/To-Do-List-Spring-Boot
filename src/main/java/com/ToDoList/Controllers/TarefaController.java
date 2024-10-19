7e231f1fb02922bae5b02a389c48c3ce73e23bed
robson_Vieira <robsonestudos883@gmail.com>
2024-10-18 22:47:36 -0300

    adicionando a documentacao com o swagger e adicionando a implementacao do controlleR

--- src/main/java/com/ToDoList/Controllers/TarefaController.java
+++ src/main/java/com/ToDoList/Controllers/TarefaController.java
@@ -1,5 +1,50 @@
 package com.ToDoList.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ToDoList.DTO.TarefaDTO;
+import com.ToDoList.Services.TarefaServices;
+
+
+@RestController
+@RequestMapping(path="tarefa")
 public class TarefaController {
 
+	@Autowired 
+	private TarefaServices service;
+	
+	@GetMapping()
+	public List<TarefaDTO> listAll() {
+		return service.buscarTodos();
+	}
+	
+	@GetMapping("/{id}")
+	public TarefaDTO foundById(@PathVariable Long id) {
+		return service.buscaPorId(id);
+	}
+	
+	@PostMapping()
+	public TarefaDTO create(@RequestBody TarefaDTO dto) {
+		return service.criarTarefa(dto);
+	}
+	
+	@PutMapping()
+	public TarefaDTO update(@RequestBody TarefaDTO dto) {
+		return  service.atualizarTarefa(dto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		service.deletarTarefa(id);
+	}
 }
